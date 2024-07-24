9df5592e80147f3b7d03aba85ac1d031da95fade
Venki8500 <167505277+Venki8500@users.noreply.github.com>
2024-07-24 12:08:48 +0530

    Add files via upload

---
+++ test/pattern2.java
@@ -0,0 +1,22 @@
+public class pattern2 {
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++){
+         
+            for(int j=7;j>=i;j--){
+            if(i%3==2){
+                System.out.print(j+" ");
+            }
+         
+            if(i%3==1){
+                System.out.print("#"+" ");
+            }
+            if(i%3==0){
+                System.out.print("$"+" ");
+            }
+            
+                
+            }
+           System.out.println(" "); 
+        }
+    }
+}
---
+++ test/pattern3.java
@@ -0,0 +1,19 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        for(int i=2 ;i<=8;i++){
+            for(int k=7;k>=i;k--){
+                System.out.print(" ");
+            }
+            for(int j=2;j<=i;j++){
+                if(i%2==1){
+                    System.out.print("#"+" ");
+                }
+                else{
+                    System.out.print(i+" ");
+                }
+       
+            }
+            System.out.println(" ");
+        }
+    }
+}
