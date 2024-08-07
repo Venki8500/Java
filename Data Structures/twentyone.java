c66d6480796c1f863fee7a93a6ad6fc75a8cba49
Venki8500 <167505277+Venki8500@users.noreply.github.com>
2024-08-07 13:27:08 +0530

    Add files via upload

---
+++ Data Structures/twentyone.java
@@ -0,0 +1,11 @@
+public class twentyone {
+    public static void main(String[] args) {
+        int[] ar={1,2,3,4,5,6,7,8,9};
+        int[] br={ar.length-1};
+        for(int i=0;i<=ar.length-1;i++){
+             br[i]=ar[i];
+            System.out.println(br[i]);
+        }
+                                          ///pending////
+    }
+}
---
+++ Data Structures/twentythree.java
@@ -0,0 +1,11 @@
+public class twentythree {
+    public static void main(String[] args) {
+        int[] ar={100,200,300,400};
+      
+        for(int i=0;i<=ar.length-1;i++){
+            double b=ar[i]-ar[i]*0.25;
+          System.out.println(b);
+          
+        }
+    }
+}
