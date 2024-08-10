421a65faad11d1aa8713870cf07cc76d0f67bced
Venki8500 <167505277+Venki8500@users.noreply.github.com>
2024-08-10 19:09:42 +0530

    Add files via upload

---
+++ Data Structures/twentyeight.java
@@ -0,0 +1,11 @@
+public class twentyeight {
+    public static void main(String[] args) {
+        int[] ar={100,99,80,45,18,200,300};
+      
+        for(int i=0;i<=ar.length-1;i++){
+            if(ar[i]>=10 && ar[i]<=99){
+                System.out.println(ar[i]);
+            }
+        }
+    }
+}
---
+++ Data Structures/twentynine.java
@@ -0,0 +1,10 @@
+public class twentynine {
+    public static void main(String[] args) {
+        int[] ar={5,10,15,16,17,18,20,25,30};
+        for(int i=0;i<=ar.length-1;i++){
+            if(ar[i]%5==0){
+                System.out.println(ar[i]);
+            }
+        }
+    }
+}
