e3092d2c18c28a069c5e53377ad1efb7cb5c46ca
Venki8500 <167505277+Venki8500@users.noreply.github.com>
2024-08-29 15:56:03 +0530

    Add files via upload

---
+++ strings/bytetostring.java
@@ -0,0 +1,7 @@
+public class bytetostring{
+    public static void main(String[] args) {
+byte [] a={75,76,77,78};
+String v=new String(a);
+System.out.println(v);
+    }
+}
---
+++ strings/chartostring.java
@@ -0,0 +1,8 @@
+public class chartostring {
+    public static void main(String[] args) {
+      char[] d={'v','e','n','k','i'};
+      String f= new String(d);
+      System.out.println(f);
+
+    }
+}
