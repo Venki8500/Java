3095b58843ca61746d70275a293523182daaee59
Venki8500 <167505277+Venki8500@users.noreply.github.com>
2024-09-03 15:47:27 +0530

    Add files via upload

---
+++ strings/stringconcatinaation.java
@@ -0,0 +1,9 @@
+public class stringconcatinaation {
+    public static void main(String[] args) {
+        String s="venki"+18+59;
+        String v=18+59+"venki";
+      
+        System.out.println(s);
+        System.out.println(v);
+    }
+}
