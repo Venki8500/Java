5c8814bed3239f87691b7b4772bfba77ca269749
Venki8500 <167505277+Venki8500@users.noreply.github.com>
2024-09-09 22:15:51 +0530

    Add files via upload

---
+++ strings level 1/six.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class six {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String x=input.nextLine();
+        String y=input.nextLine();
+        String z=input.nextLine();
+        String a=input.nextLine();
+        String b=input.nextLine();
+
+        System.out.println(x.charAt(x.length()-1));
+        System.out.println(y.charAt(y.length()-1));
+        System.out.println(z.charAt(z.length()-1));
+        System.out.println(a.charAt(a.length()-1));
+        System.out.println(b.charAt(b.length()-1));
+
+    }
+}
